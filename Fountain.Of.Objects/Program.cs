4796c60b49f32b00b059b24cff75719b6116c48b
Tanooki60 <cwhicks60@gmail.com>
2025-05-18 23:26:14 -0500

    Creation

---
+++ Fountain.Of.Objects/Program.cs
@@ -0,0 +1,22 @@
+﻿
+public class World
+{
+    public Room[,] Rooms = new Room[4, 4];
+    
+}
+
+public class Room
+{
+    public int Row { get; }
+    public int Column { get; }
+    public RoomState RoomState { get; }
+
+    public Room(int row, int column, RoomState roomState)
+    {
+        Row = row;
+        Column = column;
+        RoomState = roomState;
+    }
+}
+
+public enum RoomState {Nothing, Entrance, FountainOfObjects}

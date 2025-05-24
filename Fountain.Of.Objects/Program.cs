9a998683d310b987431d32b39bd991d55af3dd5b
Tanooki60 <165331275+Tanooki60@users.noreply.github.com>
2025-05-24 00:12:26 -0500

    Completed challenge

--- Fountain.Of.Objects/Program.cs
+++ Fountain.Of.Objects/Program.cs
@@ -4,14 +4,33 @@ public class FountainOfObjects
 {
     public static void Main()
     {
+        FountainOfObjects newGame = new FountainOfObjects();
         World world = new World();
 
         world.SetWorld();
-        //world.GetRoom(0,0);
+        
+        bool gameOver = false;
 
+        string? userInput;
+
+        do
+        {
+            GameText(world);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("What do you want to do: ");
+            
+            userInput = newGame.GetInput();
+            
+            world.Move(userInput);
+            
+            World.ActivateFountainCheck(userInput);
+
+            gameOver = newGame.HasWon(World.CurrentRoom);
+        }
+        while (!gameOver);
+        
         GameText(world);
     }
-
     private bool HasWon(Room room)
     {
         if (Room.FountainIsActive == true && (room.Column == 0 && room.Row == 0))
@@ -19,61 +38,63 @@ public class FountainOfObjects
         return false;
     }
 
+    private string? GetInput()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        return Console.ReadLine();
+    }
+
     private static void GameText(World world)
     {
         Console.ForegroundColor = ConsoleColor.DarkGreen;
         Console.WriteLine($"You are currently in the room at (Row:{World.CurrentRoom.Row}, Column:{World.CurrentRoom.Column})");
         Console.WriteLine(world.RoomStateCheck());
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write("What do you want to do: ");
     }
-
 }
-
 public class World
 {
-    public static Room[,] Rooms = new Room[4, 4];
-    public static Room CurrentRoom { get; set; }
+    public Room[,] Rooms = new Room[4, 4];
+    public static Room CurrentRoom { get; private set; }
     
     public void SetCurrentRoom(Room room) => CurrentRoom = room;
 
-    public static Room GetRoom(int row, int column)
+    private Room GetRoom(int row, int column)
     {
         return Rooms[CurrentRoom.Row + row, CurrentRoom.Column + column];
     }
 
     public void SetWorld()
     {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-            {
-                if (i == 0 && j == 0)
-                {
-                    Rooms[i, j] = new Room(0, 0, RoomState.Entrance);
-                    continue;
-                }
-                if (i == 0 && j == 2)
-                {
-                    Rooms[i, j] = new Room(0, 2, RoomState.FountainOfObjects);
-                    continue;
-                }
-                
-                Rooms[i, j] = new Room(0, 2, RoomState.Nothing);
-            }
-
+        Rooms[0, 0] = new Room(0, 0, RoomState.Entrance);
+        Rooms[0, 1] = new Room(0, 1, RoomState.Nothing);
+        Rooms[0, 2] = new Room(0, 2, RoomState.FountainOfObjects);
+        Rooms[0, 3] = new Room(0, 3, RoomState.Nothing);
+        Rooms[1, 0] = new Room(1, 0, RoomState.Nothing);
+        Rooms[1, 1] = new Room(1, 1, RoomState.Nothing);
+        Rooms[1, 2] = new Room(1, 2, RoomState.Nothing);
+        Rooms[1, 3] = new Room(1, 3, RoomState.Nothing);
+        Rooms[2, 0] = new Room(2, 0, RoomState.Nothing);
+        Rooms[2, 1] = new Room(2, 1, RoomState.Nothing);
+        Rooms[2, 2] = new Room(2, 2, RoomState.Nothing);
+        Rooms[2, 3] = new Room(2, 3, RoomState.Nothing);
+        Rooms[3, 0] = new Room(3, 0, RoomState.Nothing);
+        Rooms[3, 1] = new Room(3, 1, RoomState.Nothing);
+        Rooms[3, 2] = new Room(3, 2, RoomState.Nothing);
+        Rooms[3, 3] = new Room(3, 3, RoomState.Nothing);
+        
         CurrentRoom = Rooms[0, 0];
     }
 
-    public static void Move(string movement)
+    public void Move(string movement)
     {
-        if (CurrentRoom.Column < 3 && movement == "move north")
-            CurrentRoom = GetRoom(1, 0);
-        if (CurrentRoom.Column > 0 && movement == "move south")
-            CurrentRoom = GetRoom( -1, 0);
-        if (CurrentRoom.Row < 3 && movement == "move east")
-            CurrentRoom = GetRoom(0, 1);
-        if (CurrentRoom.Row > 0 && movement == "move west")
-            CurrentRoom = GetRoom(0, -1);
+        if (CurrentRoom.Row < 3 && movement == "move north")
+            SetCurrentRoom(GetRoom(1, 0));
+        if (CurrentRoom.Row > 0 && movement == "move south")
+            SetCurrentRoom(GetRoom( -1, 0));
+        if (CurrentRoom.Column < 3 && movement == "move east")
+            SetCurrentRoom(GetRoom(0, 1));
+        if (CurrentRoom.Column > 0 && movement == "move west")
+            SetCurrentRoom(GetRoom(0, -1));
     }
 
     public static void ActivateFountainCheck(string command)
@@ -86,21 +107,29 @@ public class World
     {
         if (CurrentRoom.RoomState == RoomState.Entrance && Room.FountainIsActive)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            return "The Fountain of Objects has been reactivated, and you have escaped with your life!";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!");
         }
 
         if (CurrentRoom.RoomState == RoomState.FountainOfObjects && Room.FountainIsActive)
-            return "You hear the rushing waters from the Fountain of Objects. It has been reactivated!";
-        if (CurrentRoom.RoomState == RoomState.FountainOfObjects)
-            return "You hear water dripping in this room. The Fountain of Objects is here!";
-        if (CurrentRoom.RoomState == RoomState.Entrance)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
+        }
+
+        if (CurrentRoom.RoomState == RoomState.FountainOfObjects && !Room.FountainIsActive)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
+        }
+
+        if (CurrentRoom.RoomState == RoomState.Entrance && !Room.FountainIsActive)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            return "You see light coming from the cavern entrance";
+            Console.WriteLine("You see light coming from the cavern entrance");
         }
         
-        return null;
+        return "------------------------------------------------------------------";
     }
 }
 
